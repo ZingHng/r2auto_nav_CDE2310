e22f6ea60f2ffa6c313b727bd881d7a37f24b17e
ZingHng <e1398185@u.nus.edu>
2025-02-11 21:13:16 +0700

    E2 Update

--- test/E2.py
+++ test/E2.py
@@ -1,24 +1,37 @@
 import time
 import RPi.GPIO as GPIO
 
-#Set pin numbering convention
-#use the Broadcom pin numbering convention
+#Set solenoid_pin numbering convention
+#use the Broadcom solenoid_pin numbering convention
 GPIO.setmode(GPIO.BCM)
 
 # Choose an appropriate pwm channel to be used to control the servo
-pin = 21
+solenoid_pin = 21
+servo_pin = 20
 
-# Set the pin as an output
-GPIO.setup(pin, GPIO.OUT)
+
+# Set the solenoid_pin as an output
+GPIO.setup(solenoid_pin, GPIO.OUT)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+p = GPIO.PWM(servo_pin, 50)
+
+p.start(7.5)
+
+a = 45
+d = (a/180)*10 + 2.5
 
 try:
     while True:
         print("HI")
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(solenoid_pin, GPIO.HIGH)
+        p.ChangeDutyCycle(d)
         time.sleep(3)
         print("BYE")
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(solenoid_pin, GPIO.LOW)
+        p.ChangeDutyCycle(2.5)
         time.sleep(1)
 
 except KeyboardInterrupt:
+    p.stop()
     GPIO.cleanup()
