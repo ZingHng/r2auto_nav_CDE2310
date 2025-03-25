a11d718df2cac94cec4ecdba5d7a575393f0432f
ZingHng <e1398185@u.nus.edu>
2025-03-25 23:48:37 +0800

    COMMIT LAST

--- survivorzonesequence.py
+++ survivorzonesequence.py
@@ -150,7 +150,7 @@ class SurvivorZoneSequence(Node):
             twist.linear.x = MAXSPEED
 #        time.sleep(0.1) # FOR VIBES, apparently
         self.publisher_.publish(twist)
-        print("PUBBED")
+        print(f"PUBBED twist.linear.x{twist.linear.x} twist.angular.z{twist.angular.z}")
         if twist.linear.x == 0.0 and twist.angular.z == 0.0:
             print("FIRE")
             self.fire_sequence()
@@ -181,7 +181,7 @@ class SurvivorZoneSequence(Node):
                     left_half, right_half = np.hsplit(pixels, 2)
                     self.approach_victim(left_half, right_half)
 
-                rclpy.spin_once(self, timeout_sec=0.1)
+                rclpy.spin_once(self) # MAYBE TIMEOUT SEC 0.1 \ timeout_sec=0.1
 
                 self.get_logger().info(f"LOOP{counter} DONE")
                 counter += 1
