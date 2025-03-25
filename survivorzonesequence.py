6a10b820ad907f9c97867ba1063754579e00cf8a
ZingHng <e1398185@u.nus.edu>
2025-03-25 22:26:29 +0800

    SURVIVOR ZONE SEQUENCE

--- survivorzonesequence.py
+++ survivorzonesequence.py
@@ -1,13 +1,8 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-"""This example is for Raspberry Pi (Linux) only!
-   It will not work on microcontrollers running CircuitPython!"""
-
 import sys
 import os
 import math
 import time
+import RPi.GPIO as GPIO
 
 import numpy as np
 import busio
@@ -20,88 +15,144 @@ import adafruit_amg88xx
 from rclpy.qos import qos_profile_sensor_data
 from nav_msgs.msg import OccupancyGrid
 
-import cmath
-
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 time.sleep(0.1)
-
-# low range of the sensor (this will be blue on the screen)
-MINTEMP = 26.0
-
-# high range of the sensor (this will be red on the screen)
-MAXTEMP = 32.0
-
-# how many color values we can have
-COLORDEPTH = 1024
 
 os.putenv("SDL_FBDEV", "/dev/fb1")
 
 # initialize the sensor
 sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
 
-# let the sensor initialize
-
-TIMERPERIOD = 0.1
 MAXSPEED = -0.05
+MAXTEMP = 32.0
 ROTATECHANGE = 0.1
-SAFETYDISTANCE = 0.230
+SAFETYDISTANCE = 0.250
+TIMERPERIOD = 0.1
+TEMPDIFFTOLERANCE = 8 #Huat
 
 class SurvivorZoneSequence(Node):
     def __init__(self):
         super().__init__('Survivor_Zone_Sequence')
-        self.temp_publisher = self.create_publisher(Float32MultiArray, 'temperature', 10)
+#        self.temp_publisher = self.create_publisher(Float32MultiArray, 'temperature', 10)
+        self.publisher_ = self.create_publisher(Twist,'cmd_vel',10)
         self.survivor_publisher = self.create_publisher(String, 'survivor', 10)
         self.survivor_sequence = False
         self.temp_grid = None
 
-    def approach_child(self):
+    def fire_sequence(self):
+        GPIO.setmode(GPIO.BCM)
+
+        in1 = 20
+        in2 = 16
+        en = 21
+        mospwm = 12
+
+        #Stepper setup
+        control_pins = [26,19,13,6]
+        for pin in control_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, 0)
+
+        fullstep_seq = [
+            [1,1,0,0],
+            [0,1,1,0],
+            [0,0,1,1],
+            [1,0,0,1]]
+
+        RUNNING = True
+        STEPSPERREV = 512
+
+        #DC motor setup
+        GPIO.setup(in1,GPIO.OUT)
+        GPIO.setup(in2,GPIO.OUT)
+        GPIO.setup(en,GPIO.OUT)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.setup(mospwm,GPIO.OUT)
+        p=GPIO.PWM(en,1000)
+        p.start(25)
+        ps = GPIO.PWM(mospwm, 1000)
+        ps.start(0)
+
+        def FlywheelStart():
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(in1,GPIO.OUT)
+            GPIO.setup(in2,GPIO.OUT)
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+
+        def FlywheelStop():
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(in1,GPIO.OUT)
+            GPIO.setup(in2,GPIO.OUT)
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.LOW)
+
+
+        def StepperTurn():
+            GPIO.setmode(GPIO.BCM)
+            x = 90 #degrees
+            angle = int((float(x)/360)*STEPSPERREV)
+
+            for i in range(angle):
+                for fullstep in range(4):
+                    for pin in range(4):
+                        GPIO.output(control_pins[pin], fullstep_seq[fullstep][pin])
+                    time.sleep(0.001)
+        
+        ps.ChangeDutyCycle(75)
+        FlywheelStart()
+        time.sleep(2)
+        StepperTurn()
+        time.sleep(4)
+        StepperTurn()
+        time.sleep(2)
+        StepperTurn()
+        time.sleep(5)
+        FlywheelStop()
+        ps.ChangeDutyCycle(0)
+        time.sleep(3)
+
+    def approach_victim(self, left, right):
         twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0
         if self.laser_range.size != 0:
             lr2i = np.nanargmin(self.laser_range)
         else:
             lr2i = 0
-        if child == left:
-            twist.angular.z -= ROTATECHANGE
-        elif child == right:
-            twist.angular.z += ROTATECHANGE
+        lr = np.sum(left) - np.sum(right)
+        if lr > TEMPDIFFTOLERANCE:
+            twist.angular.z = ROTATECHANGE
+        elif lr < -TEMPDIFFTOLERANCE:
+            twist.angular.z = -ROTATECHANGE
         elif self.laser_range[lr2i] > SAFETYDISTANCE:
             twist.linear.x = MAXSPEED
-
-        
-        twist.angular.z = ROTATECHANGE
-        time.sleep(0.5) # FOR VIBES
+#        time.sleep(0.1) # FOR VIBES, apparently
         self.publisher_.publish(twist)
-
-
-    def stopbot(self):
-        self.get_logger().info('In stopbot')
-        # publish to cmd_vel to move TurtleBot
-        twist = Twist()
-        twist.linear.x = 0.0
-        twist.angular.z = 0.0
-        # time.sleep(1)
-        self.publisher_.publish(twist)
+        if twist.linear.x == 0.0 and twist.angular.z == 0.0:
+            self.fire_sequence()
 
     def looper(self):
-        temp_msg = Float32MultiArray()
-        pixels = np.array(sensor.pixels)
-        self.temp_grid = np.reshape(pixels, 64)
-        pixel_list = self.temp_grid.tolist()
-        temp_msg.data = pixel_list
-        self.temp_publisher.publish(temp_msg)
+        while rclpy.ok():
+            pixels = np.array(sensor.pixels)
+#            temp_msg = Float32MultiArray()
+#            self.temp_grid = np.reshape(pixels, 64)
+#            pixel_list = pixels.tolist()
+#            temp_msg.data = pixel_list
+#            self.temp_publisher.publish(temp_msg)
 
-        if max(pixel_list) > MAXTEMP and not self.survivor_sequence:
-            survivor_msg = String()
-            survivor_msg.data = True
-            self.survivor_sequence = True
-            self.odom_subscription = self.create_subscription(
-                OccupancyGrid,
-                'map',
-                self.occ_callback,
-                qos_profile_sensor_data)
-            self.survivor_publisher.publish(survivor_msg)
+            if not self.survivor_sequence and np.nanargmax(pixels) > MAXTEMP:
+                survivor_msg = String()
+                survivor_msg.data = True
+                self.survivor_sequence = True
+                self.survivor_publisher.publish(survivor_msg)
 
-        if self.survivor_sequence:
+            if self.survivor_sequence:
+                left_half, right_half = np.hsplit(pixels, 4)
+                self.approach_victim(left_half, right_half)
+            rclpy.spin_once(self)
+
             
 
 def main(args=None):
@@ -109,6 +160,10 @@ def main(args=None):
 
     node_name = SurvivorZoneSequence()
     node_name.looper()
+    twist = Twist()
+    twist.linear.x = 0
+    time.sleep(0.5) # FOR VIBES
+    node_name.publisher_.publish(twist)
     node_name.destroy_node()
     rclpy.shutdown()
 
