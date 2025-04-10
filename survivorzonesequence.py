43206cf03e7fbda799d2fd4e79b231715556b58b
ZingHng <e1398185@u.nus.edu>
2025-04-10 20:01:56 +0700

    yibb

--- survivorzonesequence.py
+++ survivorzonesequence.py
@@ -144,9 +144,15 @@ class SurvivorZoneSequence(Node):
         print(time.strftime("%H:%M:%S",time.localtime()))
         if len(self.laser_range):
             closest_LIDAR_index = np.nanargmin(self.laser_range)
-            print(f"LIDAR    | closest:{np.nanmin(self.laser_range)}m @ {closest_LIDAR_index} - {closest_LIDAR_index / len(self.laser_range) * 360 }*")
-        print(f"""
+            print(f"""LIDAR    | closest:{np.nanmin(self.laser_range)}m @ {closest_LIDAR_index} - {closest_LIDAR_index / len(self.laser_range) * 360 }*
 ODOM     | roll={self.roll}, pitch={self.pitch}, yaw={self.yaw}
+TEMP     | target={max_temp}, max={np.max(sensor.pixels)}*C
+POSITION | (x, y)=({self.position[0]}, {self.position[1]})
+STORAGE  | nearestfire={self.nearest_fire}, survivor_sequence={self.survivor_sequence}
+         | activations={self.activations}
+""")
+        else:
+            print(f"""ODOM     | roll={self.roll}, pitch={self.pitch}, yaw={self.yaw}
 TEMP     | target={max_temp}, max={np.max(sensor.pixels)}*C
 POSITION | (x, y)=({self.position[0]}, {self.position[1]})
 STORAGE  | nearestfire={self.nearest_fire}, survivor_sequence={self.survivor_sequence}
